9cc1716fbcc8024489549342f4357eb50d341939
C Ferrell <ferrellchristianr@gmail.com>
2017-10-25 17:58:56 -0500

    should be complete and functional, no extras yet

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,6 +19,7 @@ var config = {
     storageBucket: "traintake2.appspot.com",
     messagingSenderId: "207933808421"
 };
+
 firebase.initializeApp(config);
 
 var database = firebase.database();
@@ -34,43 +35,76 @@ function setTable(tObj){
 
     $("#trains").append(row);
     
-    
 };
 
 function submitButton(ev){
+    
+    ev.preventDefault();
 
+    // Train name
     var name = $("#currTrain").val().trim();
+    
+    //Destination
     var dest = $("#currDest").val().trim();
-    var start = moment($("#currStrt").val().trim())._i; //as an integer
+
+    //First train time
+    var start = moment($("#currStrt").val().trim())._i;
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(start, "hh:mm").subtract(1, "years");
+
+    // Freq between trains
     var rate = parseInt($("#currRate").val().trim());
-    var nextT = moment().diff(start, "minutes");
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % rate;
+    console.log("remainder: " + tRemainder);
+
+    // Minute Until Train
+    var t_away = rate - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + t_away);
+
+    // Next Train
+    var nextT = moment().add(t_away, "minutes");
+    var nextTconvert = moment(nextT).format("hh:mm");
+    console.log("ARRIVAL TIME: " + nextTconvert);
+ 
     console.log("Train: " + name);
     console.log("Going: " + dest);
     console.log("First: " + start);
     console.log("Freq: " + rate);
-    console.log("next: " + nextT);
-
-    ev.preventDefault();
-
+  
     var trainObj = {
         t_name: name,
-        t_dest: role,
+        t_dest: dest,
         t_strt: start,
         t_rate: rate,
-        t_next: nextT,
-        t_away: (rate * nextT),
+        t_next: nextTconvert,
+        t_away: t_away,
     };
-        database.ref().push(trainObj);   
+        
+    database.ref().push(trainObj);
+
 };
 
 database.ref().on("value", function(childSnapshot){
-    //console.log(childSnapshot.val());
     var trains = childSnapshot.val();
+    console.log(trains);
+
     $("#trains").empty();
 
     for(var i in trains){
         setTable(trains[i]);
-    }
+    };
+
 });
 
 $(document).ready(function(){
